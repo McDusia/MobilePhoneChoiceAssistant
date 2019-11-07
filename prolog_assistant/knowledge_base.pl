408a249bc490655edb80b8c82d6439c111eafc76
Mikołaj Biel <mbiel@mailbox.org>
2019-11-07 15:52:49 +0100

    Add PrologPhoneChoiceAssistant and a simple rule for battery

---
+++ prolog_assistant/knowledge_base.pl
@@ -0,0 +1,5 @@
+:- dynamic
+    has/2.
+
+model("Xiaomi Redmi 5A") :- has(battery_capacity, 3000).
+model("HTC Google Pixel") :- has(battery_capacity, 2770).
---
+++ prolog_assistant/rules.pl
@@ -0,0 +1,9 @@
+:- dynamic
+    battery_threshold/1,
+    required/2.
+
+has(battery_capacity, X) :- required(battery_capacity, RequiredLevel),
+                            battery_threshold(RequiredLevel, Y),
+                            (X > Y).
+
+has(battery_capacity, _) :- not(required(battery_capacity, _)).
