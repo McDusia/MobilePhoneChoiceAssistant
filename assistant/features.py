5845e0aa6ea3d3f95a3c561097503259e5044a72
Mikołaj Biel <mbiel@mailbox.org>
2019-11-10 17:34:05 +0100

    Move CpuFrequency to assistant/translator/features

--- assistant/features.py
+++ assistant/features.py
@@ -5,8 +5,3 @@ class BatteryLife(Enum):
     EXCELLENT = "excellent"
     GOOD = "good"
     IRRELEVANT = "irrelevant"
-
-
-class CpuFrequency(Enum):
-    LOW = "low"
-    HIGH = "high"
--- assistant/translator/features.py
+++ assistant/translator/features.py
@@ -5,3 +5,8 @@ class BatteryCapacity(Enum):
     LARGE = "large"
     BIG = "big"
     OK = "ok"
+
+
+class CpuFrequency(Enum):
+    LOW = "low"
+    HIGH = "high"
