33ff36a709d0eb3e65c98273c08258766d189638
Mikołaj Biel <mbiel@mailbox.org>
2019-11-10 15:40:05 +0100

    Fail fast in has(battery_capacity, _) rule

--- prolog_assistant/rules.pl
+++ prolog_assistant/rules.pl
@@ -6,6 +6,6 @@ has(battery_capacity, X) :- required(battery_capacity, RequiredLevel),
                             battery_threshold(RequiredLevel, Y),
                             (X > Y).
 
-has(battery_capacity, _) :- not(required(battery_capacity, _)).
+has(battery_capacity, _) :- required(battery_capacity, _), !, fail.
 
 has(_, _) :- true.
