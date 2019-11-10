dd33502f3a554f3658d5b13025f01408f9716a44
Mikołaj Biel <mbiel@mailbox.org>
2019-11-10 13:48:53 +0100

    Add csv to knowledge_base translator

---
+++ translator/__main__.py
@@ -0,0 +1,27 @@
+import csv
+import sys
+from typing import TextIO
+
+import click
+
+from translator.csv_to_prolog import translate_file
+
+
+@click.command(name="translate")
+@click.argument("input-file", type=click.Path(exists=True, dir_okay=False))
+def translate(
+        input_file: str,
+):
+    """
+    Translates .csv knowledge base to Prolog
+    """
+    output_file: TextIO = sys.stdout
+
+    with open(input_file, "rt") as f:
+        reader = csv.DictReader(f)
+        for lines in translate_file(reader):
+            output_file.write(lines)
+
+
+if __name__ == '__main__':
+    translate()
