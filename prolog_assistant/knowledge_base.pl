1e15c55beb6030424b2e5faf411d1e0700de548e
Mikołaj Biel <mbiel@mailbox.org>
2019-11-10 15:46:53 +0100

    Add battery_thresholds to example knowledge_base.pl

--- prolog_assistant/knowledge_base.pl
+++ prolog_assistant/knowledge_base.pl
@@ -1,5 +1,10 @@
 :- dynamic
     has/2.
 
+model("Bluboo S3") :- has(battery_capacity, 8500).
 model("Xiaomi Redmi 5A") :- has(battery_capacity, 3000).
 model("HTC Google Pixel") :- has(battery_capacity, 2770).
+
+battery_threshold(excellent, 6999).
+battery_threshold(good, 2999).
+battery_threshold(irrelevant, 0).
