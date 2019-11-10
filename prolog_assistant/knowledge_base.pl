4b0dcc4d9ab17af8ce5ad16c0dd2b02b54d1d10f
Mikołaj Biel <mbiel@mailbox.org>
2019-11-10 17:26:18 +0100

    Update knowledge_base to reflect rules changes (cpu and battery_life)

--- prolog_assistant/knowledge_base.pl
+++ prolog_assistant/knowledge_base.pl
@@ -1,10 +1,18 @@
 :- dynamic
     has/2.
 
-model("Bluboo S3") :- has(battery_capacity, 8500).
-model("Xiaomi Redmi 5A") :- has(battery_capacity, 3000).
-model("HTC Google Pixel") :- has(battery_capacity, 2770).
+model("Bluboo S3") :-
+    has(battery_capacity, 8500),
+    has(cpu_frequency, 1500).
+model("Xiaomi Redmi 5A") :-
+    has(battery_capacity, 4500),
+    has(cpu_frequency, 1700).
+model("HTC Google Pixel") :-
+    has(battery_capacity, 2770),
+    has(cpu_frequency, 2150).
 
-battery_threshold(excellent, 6999).
-battery_threshold(good, 2999).
-battery_threshold(irrelevant, 0).
+up_threshold(battery_capacity, large, 8000).
+up_threshold(battery_capacity, big, 4000).
+up_threshold(battery_capacity, ok, 3000).
+up_threshold(cpu_frequency, high, 2000).
+down_threshold(cpu_frequency, low, 1600).
