bca2f2212d98ee9e1af30863d0035ba4a3a8de08
Mikołaj Biel <mbiel@mailbox.org>
2019-11-14 18:44:21 +0100

    Extract adding a constraint in PrologPhoneChoiceAssistant to a function

--- assistant/prolog/prolog_phone_choice_assistant.py
+++ assistant/prolog/prolog_phone_choice_assistant.py
@@ -1,3 +1,5 @@
+import string
+from typing import Any
 from typing import Dict
 from typing import Generator
 from typing import List
@@ -14,6 +16,7 @@ Rule = str
 
 
 class PrologPhoneChoiceAssistant(PhoneChoiceAssistant):
+    _REQUIRE_TEMPLATE = string.Template("required($rule_key, $value)")
 
     def __init__(self,
                  rules_file: str,
@@ -37,16 +40,22 @@ class PrologPhoneChoiceAssistant(PhoneChoiceAssistant):
 
     def battery_life(self, battery_life: BatteryLife):
         rule_key = "battery_life"
-
-        previous_rule = self._loaded_rules.get(rule_key)
-        if previous_rule:
-            self._prolog.retract(previous_rule)
-
-        new_rule = f"required(battery_life, {battery_life.name.lower()})"
-        self._prolog.asserta(new_rule)
-        self._loaded_rules[rule_key] = new_rule
+        self._require(rule_key, battery_life.name.lower())
 
     def _load_knowledge_base(self,
                              knowledge_base_file: str,
                              ):
         self._prolog.consult(knowledge_base_file)
+
+    def _require(self, rule_key: str, value: Any):
+        previous_rule = self._loaded_rules.get(rule_key)
+        if previous_rule:
+            self._prolog.retract(previous_rule)
+
+        new_rule = PrologPhoneChoiceAssistant._REQUIRE_TEMPLATE.substitute(
+            rule_key=rule_key,
+            value=value,
+        )
+
+        self._prolog.asserta(new_rule)
+        self._loaded_rules[rule_key] = new_rule
