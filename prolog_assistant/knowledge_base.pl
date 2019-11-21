517083b2efe47ae08f82c95e346dbdca53d4ec6c
justynamaciag <justynamaciag96@gmail.com>
2019-11-21 11:19:33 +0100

    Return results for battery_life and CPU_frequency

--- prolog_assistant/knowledge_base.pl
+++ prolog_assistant/knowledge_base.pl
@@ -2,9 +2,9 @@ has("Bluboo S3", battery_capacity, 8500).
 has("Bluboo S3", cpu_frequency, 1500).
 has("Xiaomi Redmi 5A", battery_capacity, 4500).
 has("Xiaomi Redmi 5A", cpu_frequency, 1700).
-has("HTC Google Pixel", battery_capacity, 2770).
+has("HTC Google Pixel", battery_capacity, 10).
 has("HTC Google Pixel", cpu_frequency, 2150).
-has("Samsung Galaxy S10", battery_capacity, 10000).
+has("Samsung Galaxy S10", battery_capacity, 10).
 has("Samsung Galaxy S10", cpu_frequency, 0).
 
 up_threshold(battery_capacity, large, 8000).
